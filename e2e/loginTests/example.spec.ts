f3b64b115112ef34b23da2632b9551fc60f54dec
hlazkov <anotherskulk@gmail.com>
2026-08-13 20:23:30 +0300

    Add Playwright configuration and initial test setup
    - Create GitHub Actions workflow for Playwright tests
    - Add Playwright configuration file
    - Implement initial test case for adding a product to the cart
    - Update package.json and package-lock.json with Playwright dependencies
    - Update .gitignore to exclude Playwright test results

---
+++ e2e/loginTests/example.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Catalog' }).click();
+  await page.locator('#product-3').click();
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await expect(page.getByRole('link', { name: 'My Cart (1)' })).toBeVisible();
+});
